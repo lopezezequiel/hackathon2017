02f00003d801588476e04c4a58abd211ccd912fc
lopezezequiel <mail@lopezezequiel.com>
2017-09-02 15:01:50 -0300

    nocomments

--- hackathon2017/hackathon2017/settingstravis.py
+++ hackathon2017/hackathon2017/settingstravis.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'qmsg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+		'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USER'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
